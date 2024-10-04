12fe8d5e8414b58a5b5381ee84c84e04c64ce483
Gustavo Queiroz <compgustavo@edu.unifil.br>
2024-10-03 21:16:52 -0300

    :sparkles: adding pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
 	plugins: [
@@ -8,5 +9,12 @@ export default defineConfig({
 		Icons({
       compiler: 'svelte',
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      }
+    })
 	]
 });
